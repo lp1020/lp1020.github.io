b072295f233f4a6288c2ed031166ff8aed74b879
Liana Penn <lianapenn@gmail.com>
2018-07-18 12:25:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 800);
 });
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -50,18 +49,17 @@ $(() => {
 
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
+
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
+
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
     modal.style.display = "none";
 }
